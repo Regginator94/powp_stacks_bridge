30e16b5ed36908723046778cb00693f188348198
Jakub Kabziński <kabza94@gmail.com>
2017-03-07 21:43:40 +0100

    Podpounkt 12 - modyfikacja kals ktore nie musza byc public

--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -37,5 +37,42 @@ public class StackList {
 		last = last.getPrev();
 		return ret;
 	}
+	
+	private class Node {
+	
+		private int value;
+		private Node prev;
+		private Node next; //W związku z hermetyzacją dostęp do pól value, prev, next jest możliwy tylko za pomocą getterów i setterów
+	
+		public Node(int i) {
+			value = i;
+		}
+	
+		public int getValue() {
+			return value;
+		}
+	
+		// public void setValue(int value)  - zostało usunięte
+	
+	
+		public Node getPrev() {
+			return prev;
+		}
+	
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+	
+		public Node getNext() {
+			return next;
+		}
+	
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
 
 }
+
+
+
