9fa6795eb6c17a1d1b9cad90818caad652d8fc1d
Jakub Kabzinski <kabza94@gmail.com>
2017-03-16 14:35:21 +0100

    Podpunkt 3.1.1

---
+++ src/main/java/edu/kis/vh/stacks/list/StackArray.java
@@ -0,0 +1,42 @@
+package edu.kis.vh.stacks.list;
+
+public class StackArray {
+	
+	private static final int EMPTY_STACK_VALUE = -1; 
+	private static final int STACK_SIZE_VALUE = 12;  
+	private static final int STACK_FULL_VALUE = 11;
+	private int[] items = new int[STACK_SIZE_VALUE]; 
+	
+	private int total = EMPTY_STACK_VALUE;
+	
+	public void push(int i) {
+		if (!isFull())
+			items[++total] = i;
+	}
+
+	public boolean isEmpty() {
+		return total == EMPTY_STACK_VALUE;
+	}
+
+	public boolean isFull() {
+		return total == STACK_FULL_VALUE;
+	}
+
+
+	public int top() {
+		if (isEmpty())
+			return EMPTY_STACK_VALUE;
+		return items[total];
+	}
+
+	public int pop() {
+		if (isEmpty())
+			return EMPTY_STACK_VALUE;
+		return items[total--];
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+}
