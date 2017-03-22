d45a7683a79c522d3d96af13f0cba48811f8c2ce
Jakub Kabziński <kabza94@gmail.com>
2017-03-22 19:11:29 +0100

    Podpunkt 3.1.11

--- src/main/java/edu/kis/vh/stacks/structures/IStack.java
+++ src/main/java/edu/kis/vh/stacks/structures/IStack.java
@@ -2,8 +2,8 @@ package edu.kis.vh.stacks.structures;
 
 public interface IStack {
 	
-	static final int EMPTY_STACK_VALUE = -1; 
-
+	static final int EMPTY_STACK_VALUE = 0; // dzięki preniesiuniu atrybutu do interfejsu wystraczy 
+										//zmienić wartość tylko w interfejsie zamiast we wszystkich klasach implementujących
 	void push(int i);
 
 	boolean isEmpty();
